45e41154476678e3f4177131e7b02bf2789fb6e2
Makara Wang <makara15@gmail.com>
2017-12-06 16:46:48 +0800

    This is how a root CA file should be used.

--- server/config.env.js
+++ server/config.env.js
@@ -11,6 +11,7 @@ const env = require('env-var');
 module.exports = {
   // Server.
   port: env.get('HELLO_PORT').asInt(),
+  rootCaFile: env.get('ROOT_CA_FILE').asString(),
 
   // Log.
   logStream: env.get('LOG_STREAM').asString(),
